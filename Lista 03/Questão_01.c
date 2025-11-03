0070332fa5478c6350cf7fa42ae822182231a1d1
Davi Feitosa <davif1605@gmail.com>
2025-11-02 23:39:52 -0300

    Início lista 03

---
+++ Lista 03/Questão_01.c
@@ -0,0 +1,7 @@
+/* Faça um programa em C que leia um vetor do tipo float com 15 elementos e apresente a soma do
+menor e maior elemento do vetor fornecido. */
+
+#include <stdio.h>
+
+#define TAM 15
+
